6397abd508c519a9f62b4f0fe9de1bd3694a7fcf
Nickolazz1 <elanir358@gmail.com>
2020-07-03 14:55:12 +0300

    test spring framework

---
+++ spring_app1/src/main/java/nickolazz1/github/io/TestBean.java
@@ -0,0 +1,17 @@
+package nickolazz1.github.io;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ spring_app1/src/main/java/nickolazz1/github/io/TestSpring.java
@@ -0,0 +1,17 @@
+package nickolazz1.github.io;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+
+        context.close();
+    }
+}
