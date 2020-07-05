d3fb091af10fba063a053cab9dec22ca16c71ee2
Nickolazz1 <elanir358@gmail.com>
2020-07-05 19:26:55 +0300

    some classes operations

--- spring_app1/src/main/java/nickolazz1/github/io/MusicPlayer.java
+++ spring_app1/src/main/java/nickolazz1/github/io/MusicPlayer.java
@@ -2,13 +2,30 @@ package nickolazz1.github.io;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
 
     //public MusicPlayer(Music music) {this.music = music;}
-
     public MusicPlayer() {}
 
     public void setMusic(Music music) {
         this.music = music;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return this.volume;
     }
 
     public void playMusic() {
