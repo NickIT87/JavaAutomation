a9c0be45689378b78153e4059952895d74bd0068
Nickolazz1 <elanir358@gmail.com>
2020-07-12 21:27:08 +0300

    some webdriver

---
+++ SDET/src/AssertionsExamples.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class AssertionsExamples {
+
+    @BeforeClass
+    void setup() {
+        System.out.println("Before Class setup ...");
+    }
+
+    @Test
+    public void testGoogleSearch() throws InterruptedException {
+        // Optional. If not specified, WebDriver searches the PATH for chromedriver.
+        System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com/");
+        Thread.sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+        driver.quit();
+    }
+
+    @AfterClass
+    void closeout() {
+        System.out.println("After Class setup ...");
+    }
+}
