f9eef64c39523a3231e11cef1f18f8a8d6e3d6d0
Nickolazz1 <elanir358@gmail.com>
2020-07-13 11:39:36 +0300

    assertions

--- SDET/src/AssertionsExamples.java
+++ SDET/src/AssertionsExamples.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -22,6 +23,7 @@ public class AssertionsExamples {
         driver.get("http://www.google.com/");
         Thread.sleep(5000);  // Let the user actually see something!
         WebElement searchBox = driver.findElement(By.name("q"));
+        Assert.assertTrue(searchBox.isDisplayed(), "searchbox is displayed");
         searchBox.sendKeys("ChromeDriver");
         searchBox.submit();
         Thread.sleep(5000);  // Let the user actually see something!
