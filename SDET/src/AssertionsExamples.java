ec1722d1d42944b8fd21ba7846ad0bb2eb233203
Nickolazz1 <elanir358@gmail.com>
2020-07-13 11:47:35 +0300

    assertions test passed

--- SDET/src/AssertionsExamples.java
+++ SDET/src/AssertionsExamples.java
@@ -10,28 +10,31 @@ import org.testng.annotations.Test;
 
 public class AssertionsExamples {
 
+    WebDriver driver;
+
     @BeforeClass
     void setup() {
         System.out.println("Before Class setup ...");
+        // Optional. If not specified, WebDriver searches the PATH for chromedriver.
+        System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://www.google.com/");
     }
 
     @Test
     public void testGoogleSearch() throws InterruptedException {
-        // Optional. If not specified, WebDriver searches the PATH for chromedriver.
-        System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://www.google.com/");
-        Thread.sleep(5000);  // Let the user actually see something!
+        //Thread.sleep(5000);  // Let the user actually see something!
         WebElement searchBox = driver.findElement(By.name("q"));
         Assert.assertTrue(searchBox.isDisplayed(), "searchbox is displayed");
         searchBox.sendKeys("ChromeDriver");
         searchBox.submit();
-        Thread.sleep(5000);  // Let the user actually see something!
-        driver.quit();
+        //Thread.sleep(5000);  // Let the user actually see something!
+
     }
 
     @AfterClass
     void tearDown() {
         System.out.println("After Class setup ...");
+        driver.quit();
     }
 }
