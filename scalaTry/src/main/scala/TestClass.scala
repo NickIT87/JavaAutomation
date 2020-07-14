aad18d41a6743188435417a861a3e0e6c337bfe5
Nickolazz1 <elanir358@gmail.com>
2020-07-14 17:51:28 +0300

    try to learn scala?

--- scalaTry/src/main/scala/TestClass.scala
+++ scalaTry/src/main/scala/TestClass.scala
@@ -1,11 +1,14 @@
 object TestClass {
   def main(args: Array[String]): Unit = {
-    val x = 21
+    val x = 20
+    var res = ""
 
     if (x == 21) {
-      println(x)
+      res = "x == 21"
     }else{
-      println("x != 20")
+      res = "x != 21"
     }
+
+    println(res)
   }
 }
