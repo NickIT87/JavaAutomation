1b0022069970e743b0adb07cf94412904536b504
NickIT87 <elanir358@gmail.com>
2023-02-05 21:00:19 +0200

    cucumber testng/junit review

--- cucumberTestNG/src/test/java/StepDefinitions/GoogleSteps.java
+++ cucumberTestNG/src/test/java/StepDefinitions/GoogleSteps.java
@@ -1,11 +1,31 @@
 package StepDefinitions;
 
+import PageObjects.SearchGooglePage;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
 
 public class GoogleSteps {
+
+    private WebDriver driver = null;
+
     @Given("google page")
     public void go_to_google_page() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-fullscreen");
+        //chromeOptions.addArguments("--headless");                         // Option for headless browser
+        chromeOptions.setImplicitWaitTimeout(Duration.ofSeconds(15));
+        // Create the driver object
+        this.driver = new ChromeDriver(chromeOptions);
+        driver.get("https://google.com");
         System.out.println("first");
     }
 
@@ -13,4 +33,5 @@ public class GoogleSteps {
     public void check_title() {
         System.out.println("second");
     }
+
 }
