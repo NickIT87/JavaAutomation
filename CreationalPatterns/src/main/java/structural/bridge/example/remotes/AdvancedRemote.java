a7b27e13fcea6f64366f2766718a627bda2df802
Nick <elanir358@gmail.com>
2025-08-18 11:05:14 +0300

    bridge pattern example

---
+++ CreationalPatterns/src/main/java/structural/bridge/example/remotes/AdvancedRemote.java
@@ -0,0 +1,15 @@
+package structural.bridge.example.remotes;
+
+import structural.bridge.example.devices.Device;
+
+public class AdvancedRemote extends BasicRemote {
+
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
---
+++ CreationalPatterns/src/main/java/structural/bridge/example/remotes/Remote.java
@@ -0,0 +1,13 @@
+package structural.bridge.example.remotes;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
