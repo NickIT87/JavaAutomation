cf98f6a63d5a203cf05f1e9064665b749180987b
Nick <elanir358@gmail.com>
2025-09-07 14:42:05 +0300

    command pattern example

---
+++ CreationalPatterns/src/main/java/behavioral/command/example/commands/CopyCommand.java
@@ -0,0 +1,16 @@
+package behavioral.command.example.commands;
+
+import behavioral.command.example.editor.Editor;
+
+public class CopyCommand extends Command {
+
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
