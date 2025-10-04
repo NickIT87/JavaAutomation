e52d7f6810afad009efc8596b496aec88c1aed70
Nick <elanir358@gmail.com>
2025-10-04 15:41:05 +0300

    state pattern example

---
+++ CreationalPatterns/src/main/java/behavioral/state/example/Demo.java
@@ -0,0 +1,15 @@
+package behavioral.state.example;
+
+import behavioral.state.example.ui.Player;
+import behavioral.state.example.ui.UI;
+
+/**
+ * Demo class. Everything comes together here.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
